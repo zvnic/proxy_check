aa59a506d6f18650b33b944517d14057b9a508db
zvnic <zvnic@mail.ru>
2024-07-15 09:01:03 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def get_ip():
+    ip_address = request.remote_addr
+    return jsonify({'ip': ip_address})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
